bb0d5b1b51b0d4cca4d1346004ecfa6f6cef934a
DianaMMartins <dian_marq_mart@hotmail.com>
2023-05-16 18:05:09 +0100

    improved navbar logic to handle current component path on creation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
+// import "../Styles/Components/Navbar.scss";
 
 const linksData = [
   { path: "/about-me", label: "About me" },
@@ -8,26 +10,33 @@ const linksData = [
   { path: "/contacts", label: "Contacts" },
 ];
 
+const createLinkData = (currentPath) => {
+  if (currentPath === "/") {
+    return [...linksData];
+  } else {
+    const newLinksData = linksData.filter((link) => link.path !== currentPath);
+    newLinksData.unshift({ path: "/", label: "Home" });
+    return newLinksData;
+  }
+};
+
 export const Navbar = () => {
-  const [navLinks, setNavLinks] = useState([...linksData]);
+  const currentPath = useLocation().pathname;
+  const [navLinks, setNavLinks] = useState(createLinkData(currentPath));
 
   const handleClick = (currentPath) => {
-    if (currentPath === "/") {
-      setNavLinks([...linksData]);
-    } else {
-      const newLinksData = linksData.filter(
-        (link) => link.path !== currentPath
-      );
-      newLinksData.unshift({ path: "/", label: "Home" });
-      setNavLinks(newLinksData);
-    }
+    setNavLinks(createLinkData(currentPath));
   };
 
   return (
     <section className="navbar">
       {navLinks.map((link) => (
-        <Link key={link.path} to={link.path} onClick={() => handleClick(link.path)}>
-          <p>{link.label}</p>
+        <Link
+          key={link.path}
+          to={link.path}
+          onClick={() => handleClick(link.path)}
+        >
+          <p className="navbar-title">{link.label}</p>
         </Link>
       ))}
     </section>
