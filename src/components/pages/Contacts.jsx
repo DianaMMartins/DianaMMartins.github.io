677aa3d752ef2265288d8ae0b353f70419c63139
DianaMMartins <dian_marq_mart@hotmail.com>
2023-05-16 11:00:11 +0100

    added contacts info

--- src/components/pages/Contacts.jsx
+++ src/components/pages/Contacts.jsx
@@ -1,14 +1,46 @@
+import { ReactComponent as Linkedin } from "../../data/images/linkedin.svg";
+import { ReactComponent as Github } from "../../data/images/github.svg";
+
 export const Contacts = () => {
-    return (
-      <section className="page scrollbox">
-        <h2>Contact me</h2>
-        <p className="scrollbox-inner">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, enim
-          quasi? Eos rem aut officia dicta quos magni molestiae? Quos natus,
-          incidunt quis exercitationem repellat temporibus asperiores nisi totam
-          earum.
-        </p>
+  return (
+    <section className="page scrollbox">
+      <h2>Contact me</h2>
+      <section className="scrollbox-inner" >
+
+      <a
+        className="link-card"
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/DianaMMartins"
+      >
+       <Github />
+      </a>
+      <a
+        className="link-card"
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.linkedin.com/in/diana-martins/"
+      >
+        <Linkedin />
+      </a>
+      <a
+        className="link-card"
+        target="_blank"
+        rel="noreferrer"
+        href="mailto:dian_marq_mart@hotmail.com"
+      >
+        Send me an email
+      </a>
+      <a
+        className="link-card"
+        target="_blank"
+        rel="noreferrer"
+        href="https://northcoders.com/projects/apr-2023/dashing-dragons"
+      >
+        See my final project from Northcoders
+      </a>
       </section>
-    );
-  };
-  
+
+    </section>
+  );
+};
