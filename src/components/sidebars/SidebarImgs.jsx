e360f531acf7792394b69646830ee4a54edd2f71
DianaMMartins <dian_marq_mart@hotmail.com>
2023-05-18 17:57:05 +0100

    changed some logic so that images animate as they change'

--- src/components/sidebars/SidebarImgs.jsx
+++ src/components/sidebars/SidebarImgs.jsx
@@ -1,42 +1,62 @@
-//create an array with images
 import images from "../../data/sidebar-images.json";
 import left from "../../data/images/leftSidebar/left.svg";
 import right from "../../data/images/rightSidebar/right.svg";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
+let pastImageIndex = 0;
 export const SidebarImages = ({ side }) => {
   const currentPath = useLocation().pathname;
   const imagesObject = images.find((object) => object.path === currentPath);
   const imagesArray = imagesObject ? imagesObject[side] : null;
-
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
   useEffect(() => {
-    if (imagesArray) {
+    if (imagesArray && imagesArray.length > 1) {
       const intervalId = setInterval(() => {
-        setSelectedImageIndex((currentIndex) => {
-          const nextIndex = currentIndex + 1;
-          return nextIndex < imagesArray.length ? nextIndex : 0;
-        });
-      }, 30000);
+        pastImageIndex = selectedImageIndex;
 
-      return () => clearInterval(intervalId);
+        const nextIndex = selectedImageIndex + 1;
+        const validatedNextIndex =
+          nextIndex < imagesArray.length ? nextIndex : 0;
+        setSelectedImageIndex(validatedNextIndex);
+
+        clearInterval(intervalId);
+      }, 5000);
     }
-  }, [currentPath, imagesArray]);
+  }, [currentPath, imagesArray, selectedImageIndex]);
 
   return (
-    <section className="image-loop">
-      {imagesArray === null ? (
-        <img src={side === "left" ? left : right} alt={side} />
-      ) : (
-        <img
-          src={require(`../../data/images${imagesArray[
-            selectedImageIndex
-          ]}`)}
-          alt={currentPath}
-        />
-      )}
+    <section className="sidebar-images">
+      {
+        imagesArray === null ? (
+          <img src={side === "left" ? left : right} alt={side} />
+        ) : imagesArray.length > 1 ? (
+          <section>
+            <img
+              src={require(`../../data/images${imagesArray[pastImageIndex]}`)}
+              alt={currentPath}
+              className="absolute"
+            />
+            <img
+              src={require(`../../data/images${imagesArray[selectedImageIndex]}`)}
+              alt={currentPath}
+              // trigger re-render component to restart animation
+              // https://stackoverflow.com/questions/63186710/how-to-trigger-a-css-animation-on-every-time-a-react-component-re-renders
+              key={selectedImageIndex}
+              className="fadeInAnimation"
+            />
+          </section>
+        ) : (
+          <img
+            src={require(`../../data/images${imagesArray[0]}`)}
+            alt={currentPath}
+            className="absolute"
+          />
+        )
+        // add fade from img1 to img2 when img2 is visible
+        // img2 should be on top of img1
+      }
     </section>
   );
 };
