b527d92063a75236f2b2ad58aa3536dc054b2b3a
DianaMMartins <dian_marq_mart@hotmail.com>
2023-05-23 17:16:42 +0100

    fixed useRef error from pastImageIndexRef

--- src/components/sidebars/SidebarImgs.jsx
+++ src/components/sidebars/SidebarImgs.jsx
@@ -1,29 +1,30 @@
 import images from "../../data/sidebar-images.json";
 import left from "../../data/images/leftSidebar/profileL.png";
 import right from "../../data/images/rightSidebar/profileR.png";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useLocation } from "react-router-dom";
 
 export const SidebarImages = ({ side }) => {
   const currentPath = useLocation().pathname;
   const imagesObject = images.find((object) => object.path === currentPath);
   const imagesArray = imagesObject ? imagesObject[side] : null;
-  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
-  let pastImageIndex = selectedImageIndex === 0 ? imagesArray.length - 1 : 0;
-
-  console.log(pastImageIndex, selectedImageIndex);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(1);
+  const pastImageIndexRef = useRef(
+    selectedImageIndex === 0 ? imagesArray.length - 1 : 0
+  );
 
   useEffect(() => {
     if (imagesArray && imagesArray.length > 1) {
       const intervalId = setInterval(() => {
-        pastImageIndex = selectedImageIndex;
+        pastImageIndexRef.current = selectedImageIndex;
 
         const nextIndex = selectedImageIndex + 1;
         const validatedNextIndex =
           nextIndex < imagesArray.length ? nextIndex : 0;
+
         setSelectedImageIndex(validatedNextIndex);
         clearInterval(intervalId);
-      }, 10000);
+      }, 2000);
     }
   }, [currentPath, imagesArray, selectedImageIndex]);
 
@@ -34,7 +35,9 @@ export const SidebarImages = ({ side }) => {
       ) : imagesArray.length > 1 ? (
         <section>
           <img
-            src={require(`../../data/images${imagesArray[pastImageIndex]}`)}
+            src={require(`../../data/images${
+              imagesArray[pastImageIndexRef.current]
+            }`)}
             alt={currentPath}
             className="absolute"
             id="img1"
@@ -42,7 +45,7 @@ export const SidebarImages = ({ side }) => {
           <img
             src={require(`../../data/images${imagesArray[selectedImageIndex]}`)}
             alt={currentPath}
-            // trigger re-render component to restart animation
+            // key trigger re-render component to restart animation
             // https://stackoverflow.com/questions/63186710/how-to-trigger-a-css-animation-on-every-time-a-react-component-re-renders
             key={selectedImageIndex}
             className="fadeInAnimation"
