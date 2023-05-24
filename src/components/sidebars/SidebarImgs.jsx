1e3051ec9bbabd0254fb40f36f08c0247cee5784
DianaMMartins <dian_marq_mart@hotmail.com>
2023-05-24 11:15:43 +0100

    fixed issue with side images when changing page

--- src/components/sidebars/SidebarImgs.jsx
+++ src/components/sidebars/SidebarImgs.jsx
@@ -1,17 +1,26 @@
 import images from "../../data/sidebar-images.json";
 import left from "../../data/images/leftSidebar/profileL.png";
 import right from "../../data/images/rightSidebar/profileR.png";
-import { useEffect, useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export const SidebarImages = ({ side }) => {
   const currentPath = useLocation().pathname;
+  const [pagePath, setPagePath] = useState(currentPath);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+  const pastImageIndexRef = useRef(0);
+
+  // ensures that the state updates are applied whenever the page path changes.
+  useEffect(() => {
+    if (pagePath !== currentPath) {
+      setPagePath(currentPath);
+      setSelectedImageIndex(0);
+      pastImageIndexRef.current = 0;
+    }
+  }, [currentPath, pagePath]);
+
   const imagesObject = images.find((object) => object.path === currentPath);
   const imagesArray = imagesObject ? imagesObject[side] : null;
-  const [selectedImageIndex, setSelectedImageIndex] = useState(1);
-  const pastImageIndexRef = useRef(
-    selectedImageIndex === 0 ? imagesArray.length - 1 : 0
-  );
 
   useEffect(() => {
     if (imagesArray && imagesArray.length > 1) {
@@ -23,13 +32,16 @@ export const SidebarImages = ({ side }) => {
           nextIndex < imagesArray.length ? nextIndex : 0;
 
         setSelectedImageIndex(validatedNextIndex);
+      }, 9000);
+      return () => {
+        console.log(clearInterval);
         clearInterval(intervalId);
-      }, 2000);
+      };
     }
-  }, [currentPath, imagesArray, selectedImageIndex]);
+  }, [imagesArray, selectedImageIndex]);
 
   return (
-    <section className="sidebar-images">
+    <section className="sidebar-images" key={pagePath}>
       {imagesArray === null ? (
         <img src={side === "left" ? left : right} alt={side} />
       ) : imagesArray.length > 1 ? (
